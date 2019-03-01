5946b7c28323a76e63165c7b7ceb52da658c90cf
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-03-01 22:35:39 +0200

    6.8

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import React from 'react'
-import { incVote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
-
+import AnecdoteList from './components/AnecdoteList'
 
 const App = (props) => {
-
- // const [newAnecdote, setAnecdote] = useState('')
-  
-  const store = props.store
-  const anecdotes = props.store.getState()
-  //console.log('Appissa props.store.getState() ', props.store.getState())
-
-
-  const vote = (id) => {
-    console.log('vote', id)
-    store.dispatch(
-      incVote(id)
-    )
-  } 
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-
-
-      <AnecdoteForm store={store} />
-
-
+      <AnecdoteList store={props.store} />
+      <AnecdoteForm store={props.store} />
     </div>
   )
 }
---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { incVote } from "../reducers/anecdoteReducer"
+
+const AnecdoteList = ( props ) => {
+
+  const store = props.store
+  const anecdotes = store.getState()
+  const vote = id => {
+    console.log("vote", id)
+    store.dispatch(incVote(id))
+  }
+
+  return (
+    <div>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button
+              onClick={() =>
+                vote(anecdote.id)
+              }
+            >
+              vote
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
